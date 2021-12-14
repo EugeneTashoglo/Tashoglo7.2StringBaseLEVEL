96a76d78c92501cf35f1062c1069226f3493afd4
Евгений Ташогло <tashoglo00@mail.ru>
2021-12-14 10:57:42 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+static class test
+{
+    static string Change(string s)
+    {
+        string res = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            res += s[i];
+            if (s[i] == '.') res += '.';
+        }
+        return res;
+    }
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(Change(s));
+    }
+}
